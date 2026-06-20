7b9a2fd1f3035376c929cddf9ef2c7f4df725fdd
JotaRdgz06 <julianrodriguezchaves@gmail.com>
2026-06-19 19:20:00 -0600

    Prueba SFML

--- Projecto ED otra vez.cpp
+++ Projecto ED otra vez.cpp
@@ -1,44 +1,21 @@
+#include <SFML/Graphics.hpp>
 
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-#include <iostream>
-#include <cstdlib>
-#include <time.h>
-#include "Graph.h"
-#include "GraphAlgorithms.h"
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
 
-using std::cout;
-using std::endl;
-
-const int NUM_NODES = 20;
-
-const int MAX_NEIGHBORS = 6;
-
-const float CONNECTION_DIST = 200.0f;
-
-int main() {
-
-	srand((unsigned)time(nullptr));
-
-	Graph g(NUM_NODES, MAX_NEIGHBORS);
-
-	g.generateRandom(CONNECTION_DIST);
-
-	cout << "Original graph:" << endl;
-
-	g.print();
-
-	cout << endl;
-
-	int startNode = 0;
-
-	Graph* tree = dfs(g, startNode);
-
-	cout << "DFS tree starting from node " << startNode << ":" << endl;
-
-	tree->print();
-
-	delete tree;	
-
-	return 0;
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
-
